7fa7e31a6147b5f6b99aed83088f46d82ed307b4
akmatoff <akmatoff.exe@gmail.com>
2024-04-29 02:10:57 +0600

    Try handling 401s

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
 import { Toaster } from "react-hot-toast";
-import { cleanInterceptors, setInterceptors } from "./requests/request";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useNotification } from "./hooks/useNotification";
 
 function App() {
-  const navigate = useNavigate();
-  const { showErrorNotification } = useNotification();
-
-  useEffect(() => {
-    setInterceptors({ navigate, showNotification: showErrorNotification });
-
-    return () => {
-      cleanInterceptors();
-    };
-  }, [navigate, showErrorNotification]);
-
   return (
     <>
       <Toaster />
--- src/queries/userdata.ts
+++ src/queries/userdata.ts
@@ -2,12 +2,27 @@ import { IUserData } from "../interfaces/auth";
 import { useQuery } from "@tanstack/react-query";
 import { QUERY_KEYS } from "../constants/queryKeys";
 import getUserData from "../requests/auth/getUserData";
+import { useEffect } from "react";
+import { useNotification } from "@/hooks/useNotification";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "@/constants/routes";
 
 export const useUserData = () => {
-  const { data, isFetching, isSuccess, isError } = useQuery<IUserData>({
+  const { data, isFetching, isSuccess, isError, error } = useQuery<IUserData>({
     queryKey: [QUERY_KEYS.USERDATA],
     queryFn: getUserData,
   });
+
+  const { showErrorNotification } = useNotification();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // @ts-expect-error wrong interface for the error, it has the response object
+    if (isError && error.status === 401) {
+      showErrorNotification("Пожалуйста авторизуйтесь");
+      navigate(ROUTES.LOGIN);
+    }
+  }, [isError, showErrorNotification, navigate, error]);
 
   return {
     data,
