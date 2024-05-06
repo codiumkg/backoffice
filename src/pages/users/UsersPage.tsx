6adbb14057c2b53256d937f4042f2033079ce50d
akmatoff <akmatoff.exe@gmail.com>
2024-05-06 16:56:28 +0600

    Remove unused vars

--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -1,9 +1,9 @@
 import ResourceList from "@/components/shared/ResourceList/ResourceList";
-import { QUERY_KEYS } from "@/constants/queryKeys";
+// import { QUERY_KEYS } from "@/constants/queryKeys";
 import { ROUTES } from "@/constants/routes";
-import { useNotification } from "@/hooks/useNotification";
-import { useUserDeletion, useUsersQuery } from "@/queries/users";
-import { useQueryClient } from "@tanstack/react-query";
+// import { useNotification } from "@/hooks/useNotification";
+import { useUsersQuery } from "@/queries/users";
+// import { useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import {
   Table,
@@ -18,22 +18,22 @@ import { columns, renderCell } from "./columns";
 function UsersPage() {
   const navigate = useNavigate();
 
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
 
   const { data: users, isLoading } = useUsersQuery();
 
-  const { showErrorNotification, showSuccessNotification } = useNotification();
+  // const { showErrorNotification, showSuccessNotification } = useNotification();
 
-  const { mutate: deleteUser, isPending } = useUserDeletion({
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.USERS] });
-      showSuccessNotification("Удалено!");
-    },
+  // const { mutate: deleteUser, isPending } = useUserDeletion({
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.USERS] });
+  //     showSuccessNotification("Удалено!");
+  //   },
 
-    onError: () => {
-      showErrorNotification();
-    },
-  });
+  //   onError: () => {
+  //     showErrorNotification();
+  //   },
+  // });
 
   return (
     <ResourceList
