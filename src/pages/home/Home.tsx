0bb596b6935896ed93e65a94b8c31924f7a4ba92
akmatoff <akmatoff.exe@gmail.com>
2024-05-23 23:05:06 +0600

    Fix teacher redirection

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import { ROUTES } from "@/constants/routes";
 import useAuth from "@/hooks/useAuth";
+import useIsTeacher from "@/hooks/useIsTeacher";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -8,13 +9,19 @@ function Home() {
 
   const { checkIsLoggedIn } = useAuth();
 
+  const isTeacher = useIsTeacher();
+
   useEffect(() => {
     if (checkIsLoggedIn()) {
-      navigate(ROUTES.REG_REQUESTS, { replace: true });
+      if (isTeacher) {
+        navigate(ROUTES.MY_GROUPS, { replace: true });
+      } else {
+        navigate(ROUTES.REG_REQUESTS, { replace: true });
+      }
     } else {
       navigate(ROUTES.LOGIN, { replace: true });
     }
-  }, [checkIsLoggedIn, navigate]);
+  }, [checkIsLoggedIn, navigate, isTeacher]);
   return <></>;
 }
 
