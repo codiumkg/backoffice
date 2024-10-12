f84df3b6bb2bc092d86168119f84ccd7004bece5
akmatoff <akmatoff.exe@gmail.com>
2024-10-12 20:51:40 +0600

    try setting preprocessor config api to modern

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern"
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "/src"),
